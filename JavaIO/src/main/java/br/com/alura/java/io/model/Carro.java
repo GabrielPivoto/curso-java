50fd4013395937fa4d9d9e222cf59efa7f424c8a
Gabriel Pivoto Ambrósio <gabriel.pivoto@inatel.br>
2022-08-25 14:19:42 -0300

    Classe model

---
+++ JavaIO/src/main/java/br/com/alura/java/io/model/Carro.java
@@ -0,0 +1,47 @@
+package br.com.alura.java.io.model;
+
+public class Carro {
+
+    private String fabricante;
+    private String modelo;
+    private String cor;
+    private double kmRodados;
+
+    public void mostraInfo(){
+        System.out.println("--------");
+        System.out.println("Modelo: " + this.modelo + " Cor: " + this.cor + " Fabricante: " + this.fabricante);
+        System.out.println("Quilometros rodados: " + this.kmRodados);
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public double getKmRodados() {
+        return kmRodados;
+    }
+
+    public void setKmRodados(double kmRodados) {
+        this.kmRodados = kmRodados;
+    }
+}
